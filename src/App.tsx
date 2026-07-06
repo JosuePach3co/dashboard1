50a53010ccc586ad9ac4808780af77e5bad647c5
JosuePach3co <150095207+JosuePach3co@users.noreply.github.com>
2026-07-06 19:45:56 +0000

    Usando interface - componentes  y props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css'
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 
 function App() {
   
   return (
       <Grid container spacing={5} sx={{ justifyContent: "left", alignItems: "center" }}>
-
+        
         {/* Encabezado */}
-        <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
-
+        <Grid size={{ xs: 12, md: 12 }}>
+          <HeaderUI/>
+        </Grid>
+        
         {/* Alertas */}
-        <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+        <Grid size={{ xs: 12, md: 12 }} container sx={{ justifyContent: "right", alignItems: "center" }}>
+          <AlertUI description= "No se preveen lluvias" />
+        </Grid>
 
         {/* Selector */}
         <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
@@ -26,7 +32,7 @@ function App() {
 
         {/* Información adicional */}
         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
-
+        
       </Grid>
    );
 }
